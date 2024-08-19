ee53c47e62dc8fd6ae333db09e47e9fbfd878ada
FelixEbobo <a.ansimov@ispsystem.com>
2024-08-19 21:28:38 +0800

    pylint fix

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,17 +1,17 @@
 import logging
 import pytest
-import pytest_asyncio
 from hamcrest import assert_that, not_none, none
 
 from gameserver.server import Server
 from gameserver.client import Client
 
-settings_path = "tests/settings.json"
+SETTINGS_PATH = "tests/settings.json"
+
 
 @pytest.mark.asyncio
 async def test_getting_login():
-    async with Server(settings_path) as server:
-        async with Client(server._settings.host, server._settings.port) as client:
+    async with Server(SETTINGS_PATH) as server:
+        async with Client(server._settings.host, server._settings.port) as client:  #  pylint: disable=protected-access
             nickname = "rickastley"
 
             await client.send_login_request(nickname)
@@ -19,8 +19,8 @@ async def test_getting_login():
 
 @pytest.mark.asyncio
 async def test_logout():
-    async with Server(settings_path) as server:
-        async with Client(server._settings.host, server._settings.port) as client:
+    async with Server(SETTINGS_PATH) as server:
+        async with Client(server._settings.host, server._settings.port) as client:  #  pylint: disable=protected-access
             nickname = "rickastley"
 
             await client.send_login_request(nickname)
@@ -34,10 +34,10 @@ async def test_logout():
 
 @pytest.mark.asyncio
 async def test_get_all_items(caplog):
-    caplog.set_level(logging.WARNING) #  Change if you need more detailed logging
+    caplog.set_level(logging.WARNING)  #  Change if you need more detailed logging
 
-    async with Server(settings_path) as server:
-        async with Client(server._settings.host, server._settings.port) as client:
+    async with Server(SETTINGS_PATH) as server:
+        async with Client(server._settings.host, server._settings.port) as client:  #  pylint: disable=protected-access
             nickname = "rickastley"
 
             await client.send_login_request(nickname)
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,18 +1,25 @@
-import pytest
 import random
-import pytest_asyncio
+
+import pytest
 from hamcrest import assert_that, equal_to, is_not, has_item, has_properties, instance_of
 
 from gameserver.server import Server
-from gameserver.misc.models import AccountLoginRequest, ItemRequest, GameSessionData, ShopItemList, ShopItem
-from gameserver.misc.errors import AccountSessionNotFound, BaseGameServerException, NotEnoughFundsInAccountBalance, AccountAlreadyOwnsItem, AccountDoesntOwnItem
+from gameserver.misc.models import AccountLoginRequest, ItemRequest, GameSessionData, ShopItemList
+from gameserver.misc.errors import (
+    AccountSessionNotFound,
+    BaseGameServerException,
+    NotEnoughFundsInAccountBalance,
+    AccountAlreadyOwnsItem,
+    AccountDoesntOwnItem,
+)
 from gameserver.misc.protocol import Protocol, ProtocolResponse
 
-settings_path = "tests/settings.json"
+SETTINGS_PATH = "tests/settings.json"
+
 
 @pytest.mark.asyncio
 async def test_getting_login():
-    async with Server(settings_path) as server:
+    async with Server(SETTINGS_PATH) as server:
         login_request = AccountLoginRequest(nickname="rickastley")
 
         game_session_data_1 = await server.login_into_account(login_request)
@@ -28,9 +35,10 @@ async def test_getting_login():
         # Check that balance adding worked only once
         assert_that(game_session_data_1.balance, equal_to(game_session_data_2.balance))
 
+
 @pytest.mark.asyncio
 async def test_logout():
-    async with Server(settings_path) as server:
+    async with Server(SETTINGS_PATH) as server:
         login_request = AccountLoginRequest(nickname="rickastley")
 
         game_session_data = await server.login_into_account(login_request)
@@ -40,16 +48,17 @@ async def test_logout():
         except BaseGameServerException as e:
             assert_that(isinstance(e, AccountSessionNotFound))
 
+
 @pytest.mark.asyncio
 async def test_get_all_item_list():
-    async with Server(settings_path) as server:
+    async with Server(SETTINGS_PATH) as server:
         shop_item_list = await server.get_all_shop_items()
         Protocol.construct(ProtocolResponse(data=shop_item_list).model_dump())
 
 
 @pytest.mark.asyncio
 async def test_buy_item():
-    async with Server(settings_path) as server:
+    async with Server(SETTINGS_PATH) as server:
         login_request = AccountLoginRequest(nickname="rickastley")
 
         game_session_data = await server.login_into_account(login_request)
@@ -80,9 +89,10 @@ async def test_buy_item():
         except BaseGameServerException as e:
             assert_that(isinstance(e, AccountAlreadyOwnsItem))
 
+
 @pytest.mark.asyncio
 async def test_sell_item():
-    async with Server(settings_path) as server:
+    async with Server(SETTINGS_PATH) as server:
         login_request = AccountLoginRequest(nickname="rickastley")
 
         game_session_data = await server.login_into_account(login_request)
@@ -103,24 +113,25 @@ async def test_sell_item():
         owned_item_list = await server.get_owned_shop_items(game_session_data.session_uuid)
         assert_that(owned_item_list, is_not(has_item(has_properties(uuid=shop_item.uuid, price=shop_item.price))))
 
-
         try:
             await server.sell_shop_item(game_session_data.session_uuid, ItemRequest(item_uuid=shop_item.uuid))
         except BaseGameServerException as e:
             assert_that(isinstance(e, AccountDoesntOwnItem))
 
+
 def test_validate():
-    # game_session_data_json = '{"data": {"account_uuid": "099e9d9c79c54a2397c8904ad903e833", "nickname": "nick", "balance": 13.52, "session_uuid": "5572db08071748bca85924bbf2cbc3fe", "owned_items": [{"uuid": "1f1ecd78d8ef4067979b38b9a4be33ee", "name": "Sampson", "price": 24, "type": "ship"}, {"uuid": "1cd5dc973a2f48b19c170558d7c4550e", "name": "garbage can", "price": 43, "type": "equipment"}]}}'
+    # pylint: disable=line-too-long
     game_session_data_json = '{"data": {"account_uuid": "099e9d9c79c54a2397c8904ad903e833", "nickname": "nick", "balance": 13.52, "session_uuid": "5572db08071748bca85924bbf2cbc3fe", "owned_items": [{"uuid": "1f1ecd78d8ef4067979b38b9a4be33ee", "name": "Sampson", "price": 24, "type": "ship"}]}}'
     game_session_data = ProtocolResponse.model_validate_json(game_session_data_json, strict=True)
     assert_that(game_session_data.data, instance_of(GameSessionData))
 
-    shop_item_list_json = '[{"uuid": "1f1ecd78d8ef4067979b38b9a4be33ee", "name": "Sampson", "price": 24, "type": "ship"}]'
+    shop_item_list_json = (
+        '[{"uuid": "1f1ecd78d8ef4067979b38b9a4be33ee", "name": "Sampson", "price": 24, "type": "ship"}]'
+    )
     shop_item_list = ShopItemList.model_validate_json(shop_item_list_json)
     assert_that(shop_item_list, instance_of(ShopItemList))
 
+    # pylint: disable=protected-access
     shop_item_list_json = '[{"uuid": "1f1ecd78d8ef4067979b38b9a4be33ee", "name": "Sampson", "price": 24, "type": "ship"}, {"uuid": "1cd5dc973a2f48b19c170558d7c4550e", "name": "garbage can", "price": 43, "type": "equipment"}]'
     shop_item_list = ShopItemList.model_validate_json(shop_item_list_json)
     assert_that(shop_item_list, instance_of(ShopItemList))
-
-
